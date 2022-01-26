4b4a1804308798e4ac9d89564d5e765bcb6ae379
Pooja Kathail <pooja.kathail@berkeley.edu>
2022-01-25 16:54:07 -0800

    also fix in R2PRofile

--- basenji/metrics.py
+++ basenji/metrics.py
@@ -338,9 +338,15 @@ class R2Profile(tf.keras.metrics.Metric):
     self._pred_sumsq = self.add_weight(name='pred_sumsq', shape=self._shape, initializer='zeros')
 
   def update_state(self, y_true, y_pred, sample_weight=None):
-    y_true = tf.concat([y_true[:,:,0],K.sum(y_true[:,:,1:], axis=-1)], axis=-1)
-    y_pred = tf.concat([y_pred[:,:,0],K.sum(y_pred[:,:,1:], axis=-1)], axis=-1)
-
+    print(y_true.shape)
+    print(y_pred.shape)
+    y_true = tf.concat([tf.expand_dims(y_true[:,:,0], axis=2),
+                        K.sum(y_true[:,:,1:], axis=-1, keepdims=True)], axis=-1)
+    y_pred = tf.concat([tf.expand_dims(y_pred[:,:,0], axis=2),
+                        K.sum(y_pred[:,:,1:], axis=-1, keepdims=True)], axis=-1)
+    print(y_true.shape)
+    print(y_pred.shape)
+    
     y_true = tf.cast(y_true, 'float32')
     y_pred = tf.cast(y_pred, 'float32')
 
